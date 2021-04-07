87d7ed5897320c8158737f46f9b75f2649da53c2
yasyaindra <indrayasya96@gmail.com>
2021-04-07 14:23:42 +0800

    switch case study

---
+++ IfElseStatement.js
@@ -0,0 +1,16 @@
+// let language = "Indonesia";
+
+// if (language == "English") {
+//   say = "Good Morning";
+// } else {
+//   say = "Selamat Pagi";
+// }
+
+// console.log(say);
+
+// Ternary
+
+const isMember = false;
+let isDiscount = isMember ? 50 : 0;
+
+console.log(`You got ${isDiscount}% discount`);
---
+++ boolean.js
@@ -0,0 +1,11 @@
+// Bolean Case
+const a = 10;
+const b = 10;
+
+let isGreater = a > b;
+let isLess = a < b;
+let isEqual = a == b;
+
+console.log(isGreater);
+console.log(isLess);
+console.log(isEqual);
